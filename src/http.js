9b0eb275dba75b0347ff09aa7fb4cb6f31af71d1
Ni Ngo <ni.ngo@asnet.com.vn>
2020-09-20 21:38:24 +0700

    Add sample for observable subscribe

--- src/http.js
+++ src/http.js
@@ -1,4 +1,16 @@
-import {add} from './helpers';
+import { add } from './helpers';
+import { Observable } from "rxjs";
 
-
-
+const o = new Observable(
+  (observer) => {
+    setTimeout(() => {
+      observer.next('From observable')
+      observer.complete()
+    }, 100)
+  }
+)
+o.subscribe({
+  next: (message) => add.li(message),
+  error: (error) => console.log(error),
+  complete: () => add.li("This Observable is complete")
+})
