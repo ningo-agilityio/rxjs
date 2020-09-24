ea9046ffd22b9565d1da15c946ca9ae3380565b2
Ni Ngo <ni.ngo@asnet.com.vn>
2020-09-25 06:04:10 +0700

    Add sample for filter and map

--- src/operators.js
+++ src/operators.js
@@ -1,7 +1,7 @@
 import { add } from './helpers';
 import { of, from, fromEvent, interval } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, filter, take, map } from 'rxjs/operators';
 
 // Fetch
 const endpoint = 'https://jsonplaceholder.typicode.com/users'
@@ -18,3 +18,9 @@ fromEvent(btnSubmit, 'click').subscribe(() => add.li('clicked', 'display-list-op
 // Simple
 const numbers = of(1, 2, 3, 4).subscribe(value => add.li(value, 'display-list-operators'))
 const fruits = from(['apple', 'orange', 'grape']).subscribe(value => add.li(value, 'display-list-operators'))
+
+// filter, map
+const numbersArr = ["zero", "one", "two", "three", "four"]
+const counter = interval(1000).pipe(take(4))
+counter.pipe(filter(value => value % 2 === 0), map(value => numbersArr[value]))
+.subscribe(value => add.li(value, "display-list-operators"))
