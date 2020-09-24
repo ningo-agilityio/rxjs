bef39e19ab71afb5f0c8b61e63815587ead1a895
Ni Ngo <ni.ngo@asnet.com.vn>
2020-09-25 06:27:06 +0700

    Add sample for pluck and tap

--- src/operators.js
+++ src/operators.js
@@ -1,15 +1,16 @@
 import { add } from './helpers';
 import { of, from, fromEvent, interval } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
-import { switchMap, filter, take, map } from 'rxjs/operators';
+import { switchMap, filter, take, map, tap, pluck } from 'rxjs/operators';
 
 // Fetch
 const endpoint = 'https://jsonplaceholder.typicode.com/users'
 const users = fromFetch(endpoint)
 .pipe(switchMap((response) => response.json()))
-.subscribe((result) => result.forEach(
-  user => add.li(user.name)
-))
+.subscribe((result) => from(result).
+  pipe(pluck("name"))
+  .subscribe(value => add.li(value, 'display-list-operators')) 
+)
 
 // Event
 const btnSubmit = document.getElementById('btnSubmit')
@@ -21,6 +22,9 @@ const fruits = from(['apple', 'orange', 'grape']).subscribe(value => add.li(valu
 
 // filter, map
 const numbersArr = ["zero", "one", "two", "three", "four"]
-const counter = interval(1000).pipe(take(4))
-counter.pipe(filter(value => value % 2 === 0), map(value => numbersArr[value]))
+const counter = interval(1000)
+counter.pipe(take(4), filter(value => value % 2 === 0), map(value => numbersArr[value]))
+.subscribe(value => add.li(value, "display-list-operators"))
+
+counter.pipe(take(5), tap(value => add.li(`before x 2: ${value}`, "display-list-operators")), map(value => value * 2), tap(value => add.li(`after x 2: ${value}`, "display-list-operators")))
 .subscribe(value => add.li(value, "display-list-operators"))
