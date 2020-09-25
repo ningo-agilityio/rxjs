a7358d31d12ae356c8b7318366b70a4ed43d89eb
Ni Ngo <ni.ngo@asnet.com.vn>
2020-09-25 10:32:07 +0700

    Add sample for scan and reduce

--- src/operators.js
+++ src/operators.js
@@ -6,7 +6,12 @@ import {
   filter, 
   take, takeWhile,
   map, tap, pluck, startWith, 
-  skipLast, skipWhile 
+  skipLast, skipWhile,
+  throttle, 
+  debounceTime,
+  scan,
+  reduce,
+  concatMap 
 } from 'rxjs/operators';
 
 // Fetch
@@ -18,7 +23,7 @@ const ignoreUser = {
 }
 
 const endpoint = 'https://jsonplaceholder.typicode.com/users'
-const users = fromFetch(endpoint)
+fromFetch(endpoint)
 .pipe(switchMap((response) => response.json()))
 .subscribe((result) => from(result).
   pipe(
@@ -36,8 +41,8 @@ const btnSubmit = document.getElementById('btnSubmit')
 fromEvent(btnSubmit, 'click').subscribe(() => add.li('clicked', 'display-list-operators'))
 
 // Simple
-const numbers = of(1, 2, 3, 4).subscribe(value => add.li(value, 'display-list-operators'))
-const fruits = from(['apple', 'orange', 'grape']).subscribe(value => add.li(value, 'display-list-operators'))
+of(1, 2, 3, 4).subscribe(value => add.li(value, 'display-list-operators'))
+from(['apple', 'orange', 'grape']).subscribe(value => add.li(value, 'display-list-operators'))
 
 // filter, map
 const numbersArr = ["zero", "one", "two", "three", "four"]
@@ -47,3 +52,53 @@ counter.pipe(take(4), filter(value => value % 2 === 0), map(value => numbersArr[
 
 counter.pipe(take(5), tap(value => add.li(`before x 2: ${value}`, "display-list-operators")), map(value => value * 2), tap(value => add.li(`after x 2: ${value}`, "display-list-operators")))
 .subscribe(value => add.li(value, "display-list-operators"))
+
+// Throttle
+interval(10).pipe(throttle(() => interval(1000)), take(10)).subscribe(value => add.li(value, "display-list-throttle"))
+
+// Debounce
+const textBox = document.getElementById("input-debounce")
+const renderBox = document.getElementById("display-content-debounce")
+const btnSubmitDebounce = document.getElementById("btn-debounce")
+
+const content = fromEvent(textBox, 'keyup')
+const submitEvent = fromEvent(btnSubmitDebounce, 'click')
+content
+.pipe(debounceTime(500))
+.subscribe(() => {
+  renderBox.innerHTML = textBox.value
+})
+
+// Scan
+interval(100)
+.pipe(
+  take(5),
+  scan(
+    (arr, value) => {
+      const n = value + 1
+      const last = arr[n]
+      const beforeLast = arr[n-1]
+
+      return [...arr, last + beforeLast]
+    },
+    [0, 1]
+  )
+)
+.subscribe(value => add.li(value, "display-list-scan"))
+
+// Reduce
+interval(100)
+.pipe(
+  take(5),
+  reduce(
+    (arr, value) => {
+      const n = value + 1
+      const last = arr[n]
+      const beforeLast = arr[n-1]
+
+      return [...arr, last + beforeLast]
+    },
+    [0, 1]
+  )
+)
+.subscribe(value => add.li(value, "display-list-reduce"))
