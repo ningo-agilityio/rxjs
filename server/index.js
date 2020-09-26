92c39b6367c549dbe118011987acae448448b9e0
Ni Ngo <ni.ngo@asnet.com.vn>
2020-09-27 06:47:39 +0700

    Ignore .env file

--- server/index.js
+++ server/index.js
@@ -13,3 +13,52 @@ const keys = {
   //https://darksky.net/dev
   darkSky : "YOUR KEY GOES HERE"
 };
+
+const googleMapsClient = require("@google/maps").createClient({
+  key: keys.googleMaps
+});
+const util = require("@google/maps").util;
+
+const token = util.placesAutoCompleteSessionToken();
+
+
+app.get("/autocomplete/:id", (req, res) => {
+  
+
+  googleMapsClient.placesAutoComplete(
+    {
+      input: req.params.id,
+      language: "en",
+      sessiontoken: token
+    },
+    (_, result) => {
+      res.send(result.json.predictions);
+    }
+  );
+});
+
+app.get("/place/:id", (req, res) => {
+  googleMapsClient.place(
+    {
+        placeid: req.params.id,
+        language: 'en'
+    },
+    (_, result) => {
+        
+      res.send(result.json.result);
+    }
+  );
+});
+
+
+
+app.get("/weather/:lat/:long", (req, res) => {
+    fetch(`https://api.darksky.net/forecast/${keys.darkSky}/${req.params.lat},${req.params.long}`)
+    .then(res => res.json())
+    .then(json => res.json(json));
+
+
+});
+
+
+app.listen(port, () => console.log(`The Weather Server is running on ${port}!`));
