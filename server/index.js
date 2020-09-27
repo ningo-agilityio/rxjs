dc3a4e67769465e3c2872f2a33d465a86caf7f63
Ni Ngo <ni.ngo@asnet.com.vn>
2020-09-27 17:47:04 +0700

    Add sample for place photo

--- server/index.js
+++ server/index.js
@@ -51,8 +51,7 @@ app.get("/place/:id", (req, res) => {
   );
 });
 
-
-
+// Place photo: https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=YOUR_API_KEY
 app.get("/weather/:lat/:long", (req, res) => {
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${req.params.lat}&lon=${req.params.long}&appid=${keys.openWeather}`)
     .then(res => res.json())
